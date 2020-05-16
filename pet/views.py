0295754bd11e807f41ae4adfcc75bbce393fba7f
Jarquevious <jarquevious@gmail.com>
2020-05-15 20:31:29 -0700

    views in progress

--- pet/views.py
+++ pet/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 # Create your views here.
