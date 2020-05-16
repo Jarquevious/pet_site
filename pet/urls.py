07558aa5b34a0c9b9fc9e6a8a387c0f9fcd9a3c3
Jarquevious <jarquevious@gmail.com>
2020-05-15 21:28:15 -0700

    petsite templates added

--- pet/urls.py
+++ pet/urls.py
@@ -1,4 +1,12 @@
+from . import views
 from django.urls import path
+from pets.views import HomePage, PetCreateView, PetListView, PetDetailView, AppointmentSchedule, CalendarView
 
 urlpatterns = [
-    path('/', ),
+    path('', Home.as_view(), name='home'),
+    path('pets/', PetListView.as_view(), name='pets-list-page'),
+    path('pet/create/', PetCreateView.as_view(), name='pet-create-page'),
+    path('pets/<int:pet_id>/', PetDetailView.as_view(), name='pet-detail-page'),
+    path('schedule/', CalendarView.as_view(), name='calendar-list'),
+    path('schedule/createtime', AppointmentCreation.as_view(), name='calendar-create'),
+]
--- pet/views.py
+++ pet/views.py
@@ -2,4 +2,42 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.utils import timezone
+from django.http import HttpResponse 
+from pets.models import Pet, Appointment
+
 # Create your views here.
+class Home(ListView):
+    def get(self, request):
+        return render(request, 'home.html')
+
+class Pet_List_Page(ListView):
+    model = Pet
+    
+    def get(self, request):
+        pets = self.get_queryset().all()
+        return render(request, 'pet/PetsList.html', {'pets': pets})
+
+class PetCreateView(CreateView):
+    model = Pet
+    fields = ['owner', 'pet_name', 'species', 'breed', 'weight_in_lbs']
+    template_name = 'pet/Create_Pet.html'
+
+class PetDetailView(DetailView):
+    def get(self, request, pet_id):
+        return render(request, 'pet/Pet_Detail.html', {'pet': Pet.objects.get(id=pet_id)})
+
+class AppointmentScheduler(CreateView):
+    model = Appointment
+    fields = ['date_of_appointment', 'duration_minutes', 'special_instructions', 'pet']
+    template_name = 'schedule/ScheduleAppointmentPage.html'
+
+
+class CalendarView(ListView):
+    model = Appointment
+
+    def get(self, request):
+        appointments = self.get_queryset().all()
+        return render(request, 'schedule/Appointment_List.html', {'appointments': appointments.filter(
+            date_of_appointment__gte=timezone.now()
+        ).order_by('date_of_appointment', 'duration_minutes')})
