064859fcae53e7f3267c8d3394a0aa9148babaf1
Jarquevious <jarquevious@gmail.com>
2020-05-15 20:06:27 -0700

    models added not migrated

--- pet/models.py
+++ pet/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Pet(models.Model):
+    pet_name = models.CharField(max_length=200)
+    species = models.CharField(max_length=200)
+    breed = = models.CharField(max_length=20=)
+    weight_in_lbs = models.IntegerField(default=0)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True, help_text='The user who posted this question.')
+
+    def __str__(self):
+        self.pet_name
+
+class Appointment(models.Model):
+    date_of_appointment = models.DateTimeField('Appointment Date')
+    duration_minutes = models.IntegerField(default=0)
+    special_instructions = models.CharField(max_length=600)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        self.pet.pet_name
